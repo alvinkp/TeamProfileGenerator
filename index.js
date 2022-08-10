b6a2245d9615d4537b805128550de02002db06ee
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-08-09 21:52:07 -0400

    Update index.js
    *JS*
    
    - Added Dependencies to index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// Include dependencies
+const fs = require('fs/promises');
+const inquirer = require('inquirer');
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+
+// Use Inquirer to ask questions so HTML Generator can be called
+
+// Call Generate HTML function from external file
+
+// Write File to Dist folder
+
+// Init Function
+
+// Init Call
