1faf1175cd185a88b969410648d4500076dd93a6
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-08-10 00:12:32 -0400

    Update index.js
    *JS*
    
    - Added Inquirer Question functions (updateManager(), addMoreTeammates(),
     getAndStoreEngineers(),
     getAndStoreInterns())
    
    - Added generateHTML()

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const Intern = require("./lib/Intern.js");
 
 // Variables
 let currentID = 0;
-let myManager;
+let myTeam = [];
+
+// Update Manager
+function updateManager(answer){
+    let myManager = new Manager(answer.mName, getCurrentID(), answer.mOffice, answer.mEmail);
+    myTeam.push(myManager);
+}
 
 // Use Inquirer to ask questions so HTML Generator can be called
 const introQuestions = [
@@ -28,12 +34,63 @@ const internQuestions = [
     { type: 'input', message: 'What is their School?', name: 'iSchool'},
 ];
 
+const addMoreQuestions = [
+    { type: 'list', message: 'Which team member would you like to add?', name: 'selection', choices: ['An Engineer', 'An Intern', 'None, I am finished']},
+];
+
 // Return Current ID 
 function getCurrentID(){
     return currentID += 1;
 }
 
+// Ask if the Manager wants to add an Engineer, Intern or if they are finished adding Team Members
+function addMoreTeammates(){
+    inquirer
+    .prompt(addMoreQuestions)
+    .then((answers) => {
+        switch(answers.selection){
+            
+            case 'An Engineer': getAndStoreEngineers();
+            break;
+
+            case 'An Intern': getAndStoreInterns();
+            break;
+
+            case 'None, I am finished': generateHTML();
+            break;
+
+            default: console.log("Something Went Wrong");
+        }
+    })
+} 
+
+// Ask Engineer Questions and Store Engineers
+function getAndStoreEngineers(){
+    inquirer
+    .prompt(engineerQuestions)
+    .then((answers) => {
+        let myEngineer = new Engineer(answers.eName, getCurrentID(), answers.eGithub, answers.eEmail);
+        myTeam.push(myEngineer);
+        addMoreTeammates();
+    })
+}
+
+// Ask Intern Questions and Store Interns
+function getAndStoreInterns(){
+    inquirer
+    .prompt(internQuestions)
+    .then((answers) => {
+        let myIntern = new Intern(answers.iName, getCurrentID(), answers.iSchool, answers.iEmail);
+        myTeam.push(myIntern);
+        addMoreTeammates();
+    })
+}
+
 // Call Generate HTML function from external file
+function generateHTML(){
+    console.log("Generate HTML Please!");
+    console.log(myTeam);
+}
 
 // Write File to Dist folder
 
@@ -42,8 +99,8 @@ function Init(){
     inquirer
     .prompt(introQuestions)
     .then((answers) => {
-        myManager = new Manager(answers.mName, getCurrentID(), answers.mOffice, answers.mEmail);
-        console.log(myManager);
+        updateManager(answers);
+        addMoreTeammates();
     })
 }
 
